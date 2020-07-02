6a2a141b6c9a505d29b4383228f8b09a8ffc2811
gahoisathi@gmail.com <Gahoi@123#$>
2020-07-02 15:24:32 +0530

    fc

---
+++ app/src/main/java/com/sujeet/logindesign/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.sujeet.logindesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        ((TextView)findViewById(R.id.loginBtn)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+            }
+        });
+
+    }
+}
